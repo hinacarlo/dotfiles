4a798486c8d4f9c6b769776b5bbe467519d13a93
hinacarlo <hinacarlo1993@gmail.com>
2022-09-02 10:37:22 +0800

    Luasnip setup

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,6 +1,39 @@
 local status, cmp = pcall(require, "cmp")
 if (not status) then return end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then return end
+
 local lspkind = require 'lspkind'
 
-cmp.setup({})
-
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }
+})

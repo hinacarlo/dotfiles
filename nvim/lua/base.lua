e556e098769e9f82ad698a48fc6654e5b5786ecc
hinacarlo <hinacarlo1993@gmail.com>
2022-09-03 13:00:54 +0800

    Colorscheme

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -57,7 +57,7 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
--- vim.cmd [[colorscheme catppuccin]]
+vim.cmd [[colorscheme catppuccin]]
 --vim.cmd [[colorscheme gruvbox]]
 --vim.cmd [[colorscheme nightfox]]
 vim.cmd "set whichwrap+=<,>,[,],h,l"
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -106,11 +106,6 @@ _G.packer_plugins = {
     path = "/home/carlo/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["colorbuddy.nvim"] = {
-    loaded = true,
-    path = "/home/carlo/.local/share/nvim/site/pack/packer/start/colorbuddy.nvim",
-    url = "https://github.com/tjdevries/colorbuddy.nvim"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/carlo/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -135,11 +130,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/carlo/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
-  },
-  ["neosolarized.nvim"] = {
-    loaded = true,
-    path = "/home/carlo/.local/share/nvim/site/pack/packer/start/neosolarized.nvim",
-    url = "https://github.com/svrana/neosolarized.nvim"
   },
   ["nightfox.nvim"] = {
     loaded = true,
